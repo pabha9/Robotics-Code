bc4cc73e1ef2c66a8301f69f3c2cccbe37e6b3f1
pabha9 <pabha9@yahoo.com>
2016-04-08 20:54:45 -0700

    Update IR.ino

--- IR.ino
+++ IR.ino
@@ -1,54 +1,94 @@
-source: http://circuitdigest.com/microcontroller-projects/line-follower-robot-using-arduino
 
-/*------ Program for Line Follower Robot using Arduino----- */
-/*-------definning Inputs------*/
-#define LS 2      // left sensor
-#define RS 3      // right sensor
-/*-------definning Outputs------*/
-#define LM1 4       // left motor
-#define LM2 5       // left motor
-#define RM1 6       // right motor
-#define RM2 7       // right motor
-void setup()
-{
-  pinMode(LS, INPUT);
-  pinMode(RS, INPUT);
-  pinMode(LM1, OUTPUT);
-  pinMode(LM2, OUTPUT);
-  pinMode(RM1, OUTPUT);
-  pinMode(RM2, OUTPUT);
+//Code for the QRE1113 Digital board
+//Outputs via the serial terminal - Lower numbers mean more reflected
+//3000 or more means nothing was reflected.
+//Authors: Daniel and Huong
+
+int IR1 = 6; //connected to digital 6
+int IR2 = 7;
+int IR3 = 8;
+int IR4 = 9;
+/*int LED1 = 12;  // move backwards
+int LED2 = 11;  // move fowards
+int LED3 = 10;  // move right
+int LED4 = 9;   // move left*/
+int leftMotor = 10;//left port
+int rightMotor = 11; //right port 
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(leftMotor, OUTPUT);
+  pinMode(rightMotor, OUTPUT);
+ // pinMode(LED3, OUTPUT);
+  //pinMode(LED4, OUTPUT);
 }
-void loop()
-{
-  if(digitalRead(LS) && digitalRead(RS))     // Move Forward
+
+
+void loop(){
+  int LS1 = readQD(IR1);
+  int LS2 = readQD(IR2);
+  int RS1 = readQD(IR3);
+  int RS2 = readQD(IR4);
+  Serial.println(LS1); 
+
+
+
+
+  if (LS1<1000 || LS2<1000)
   {
-    digitalWrite(LM1, HIGH);
-    digitalWrite(LM2, LOW);
-    digitalWrite(RM1, HIGH);
-    digitalWrite(RM2, LOW);
+    if (LS1<1000 && RS1<1000)
+    // Move backwards if front sees white
+    {
+      digitalWrite(LED1, HIGH);
+      delay(80);
+      digitalWrite(LED1, LOW);
+    }
+
+    else if (LS2<1000 && RS2<1000)
+    // Move foward if back sees white
+    {
+      digitalWrite(LED2, HIGH);
+      delay(80);
+      digitalWrite(LED2, LOW);
+    }
+
+    else
+    // Spin right if ONLY one of the front sensors see white
+    {
+      digitalWrite(LED3, HIGH);
+      delay(80);
+      digitalWrite(LED3, LOW);
+    }
   }
   
-  if(!(digitalRead(LS)) && digitalRead(RS))     // Turn right
+
+  else
   {
-    digitalWrite(LM1, LOW);
-    digitalWrite(LM2, LOW);
-    digitalWrite(RM1, HIGH);
-    digitalWrite(RM2, LOW);
-  }
-  
-  if(digitalRead(LS) && !(digitalRead(RS)))     // turn left
-  {
-    digitalWrite(LM1, HIGH);
-    digitalWrite(LM2, LOW);
-    digitalWrite(RM1, LOW);
-    digitalWrite(RM2, LOW);
-  }
-  
-  if(!(digitalRead(LS)) && !(digitalRead(RS)))     // stop
-  {
-    digitalWrite(LM1, LOW);
-    digitalWrite(LM2, LOW);
-    digitalWrite(RM1, LOW);
-    digitalWrite(RM2, LOW);
+    if (RS1<1000 && LS1<1000)
+    // Move foward if the BOTH front sensors see white
+    {
+      digitalWrite(LED2, HIGH);
+      delay(80);
+      digitalWrite(LED2, LOW);
+    }
   }
 }
+
+
+int readQD(int sensor_number){
+  //Returns value from the QRE1113 
+  //Lower numbers mean more refleacive
+  //More than 3000 means nothing was reflected.
+  pinMode( sensor_number, OUTPUT );
+  digitalWrite( sensor_number, HIGH );  
+  delayMicroseconds(10);
+  pinMode( sensor_number, INPUT );
+
+  long time = micros();
+
+  //time how long the input is HIGH, but quit after 3ms as nothing happens after that
+  while (digitalRead(sensor_number) == HIGH && micros() - time < 3000); 
+  int diff = micros() - time;
+
+  return diff;
+}  
